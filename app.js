a0de5bebdb266a0e520c1eed9f0aa84666160da4
Rooted1 <ruthobey@gmail.com>
2016-12-07 23:33:56 +0100

    implement signout functionality

--- app.js
+++ app.js
@@ -125,6 +125,11 @@ app.post('/signup', (req, res) => {
           }
 });
 
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+})
+
 app.post('/dashboard', (req, res) => {
         console.log(JSON.stringify(req.body));
         
