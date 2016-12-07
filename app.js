e8d5aedeee97f6c264c3a8c911d850efbae05d73
Rooted1 <ruthobey@gmail.com>
2016-12-07 10:59:52 +0100

    use ejs template to display book details on iteration

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
 const express = require('express');
+const firebase = require('firebase');
+require('firebase/auth');
+require('firebase/database');
 const path = require('path');
 const app = express();
 const engine = require('ejs-locals');
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('static'));
-app.engine('ejs', engine)
+app.engine('ejs', engine);
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCfONITbfT3fLf2miCa4ryLqdAdlr7xq7g",
+    authDomain: "library-application-d0904.firebaseapp.com",
+    databaseURL: "https://library-application-d0904.firebaseio.com",
+    storageBucket: "library-application-d0904.appspot.com",
+    messagingSenderId: "121811453489"
+  };
+firebase.initializeApp(config);
+const database = firebase.database();
 
 app.get('/', (req, res) => {
-
         const name = "University Library";
-        
-         res.render("login.ejs",  { sendNameToTemplate: name});
+        res.render('login.ejs');
     });
 
 app.get('/borrowed', (req, res) => {
-        
-         res.render('borrowed.ejs');
-        
+        var books = [];
+        const a = database.ref('books').once('value').then( (snapshot) => {
+            var data = snapshot.val()
+            books.push(data)
+        });
+         res.render("borrowed.ejs",  { books: books});
     });
 
 app.get('/dashboard', (req, res) => {
-        const name = "Welcome to Admin Dashboard";
-        
-         res.render("dashboard.ejs",  { sendNameToTemplate: name});
+        var books = [];
+        const a = database.ref('books').once('value').then( (snapshot) => {
+            var data = snapshot.val()
+            books.push(data)
+        });
+         res.render("dashboard.ejs",  { books: books});
     });
 
+app.post('/dashboard', (req, res) => {
 
-app.listen(3000, function(){
+      var newBookDetails = new Book({ bookId: req.body.book_id, bookCategory: req.body.book_category, bookTitle: req.body.book_title, bookAuthor: req.body.book_author, qty: req.body.book_qty });
+
+        newBookDetails.save((err, firstResponse) => { 
+    res.redirect('/dashboard');
+});
+});
+
+app.listen(3000, () => {
     console.log('listening on port 3000')
 })
---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+$("#loginForm").submit(function(e){
+e.preventDefault();
+var email = e.currentTarget.email.value;
+var password = e.currentTarget.password.value
+  
+})
+});
